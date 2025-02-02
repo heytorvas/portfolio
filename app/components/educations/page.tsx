1fefd40a1b4f5d3f43a64a3999db12852398b0de
Heytor Victor <heytor@heytor.dev>
2025-02-02 16:52:01 -0300

    fix: remove project section

--- app/components/educations/page.tsx
+++ app/components/educations/page.tsx
@@ -15,7 +15,7 @@ const Educations = () => {
 				Educations
 			</Title>
 
-			<div className="pb-14 border-b border-slate-300 mb-14">
+			<div className="pb-14 border-b border-slate-300">
 				{data.map((education) => {
 					return (
 						<div className="flex gap-4 py-6" key={education.id}>
--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,6 @@ export default async function Home() {
 			<Experiences />
 
 			<Educations />
-
-			<Projects />
 		</main>
 	);
 }
